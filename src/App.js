19cbdf38af2bf04df4355892b8d00e75fe351f19
Nathan McDonald <nmcdonald00@gmail.com>
2019-10-11 16:34:38 -0300

    added sass styling and fonts

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
+import "./App.scss";
+
 import React from "react";
 
 function App({ title }) {
-  return <div>{title}</div>;
+  return <div className="projectname-app__container">{title}</div>;
 }
 
 export default App;
--- webpack.config.js
+++ webpack.config.js
@@ -7,10 +7,30 @@ module.exports = {
       {
         test: /\.(js|jsx)$/,
         exclude: "/node_modules/",
-        use: ["babel-loader", "eslint-loader"]
+        use: [
+          { loader: "babel-loader", options: { sourceMap: true } },
+          "eslint-loader"
+        ]
+      },
+      {
+        test: /\.scss$/,
+        loader: "style-loader!css-loader!sass-loader"
+      },
+      {
+        test: /\.(woff(2)?|ttf|eot|svg)(\?v=\d+\.\d+\.\d+)?$/,
+        use: [
+          {
+            loader: "file-loader",
+            options: {
+              name: "[name].[ext]",
+              outputPath: "fonts/"
+            }
+          }
+        ]
       }
     ]
   },
+  devtool: "source-map",
   resolve: {
     extensions: ["*", ".js", ".jsx"]
   },
