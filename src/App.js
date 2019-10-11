30a3a77f4643c599bc431717b217d277afe58915
Nathan McDonald <nmcdonald00@gmail.com>
2019-10-11 16:47:01 -0300

    added image loading to webpack

--- src/App.js
+++ src/App.js
@@ -2,8 +2,15 @@ import "./App.scss";
 
 import React from "react";
 
+import catImage from "./assets/images/cat.jpg";
+
 function App({ title }) {
-  return <div className="projectname-app__container">{title}</div>;
+  return (
+    <div className="projectname-app__container">
+      {title}
+      <img src={catImage} />
+    </div>
+  );
 }
 
 export default App;
