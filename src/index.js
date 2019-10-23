1256d6293cf92faa2cc718f632232c54d4405736
Nathan McDonald <nmcdonald00@gmail.com>
2019-10-23 06:43:58 -0300

    added env variables to prod and dev builds

--- src/index.js
+++ src/index.js
@@ -11,6 +11,8 @@ ReactDOM.render(<App title={title} />, document.getElementById("app"));
 
 module.hot.accept();
 
+console.log(process.env.NODE_ENV);
+
 //TODO
 /*add button mixin, add production build to web pack*/
 // add testing to webpack
